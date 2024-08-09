6dbb796a404d99583bb602d099d1cc8778f4df43
FahdAzouz <fahdstudies00@gmail.com>
2024-08-09 19:47:26 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
 export default defineConfig({
-    integrations: [tailwind(), react()],
-    vite: {
-        ssr: {
-          external: ["svgo"],
-        },
-      },
+  site: 'https://fahdazouz.github.io',
+  integrations: [tailwind(), react()],
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
 });
