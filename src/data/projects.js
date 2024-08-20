a3f52e1eed60dd2a0c48cb8166a3236b70608cef
FahdAzouz <fahdstudies00@gmail.com>
2024-08-20 21:53:06 +0100

    added project contents

---
+++ src/data/projects.js
@@ -0,0 +1,66 @@
+export default [
+    {
+        title: "IEEE Event Website",
+        slug: "ieee-event-website",
+        image: "/images/projects/projectBgFiller3.png",
+        shortDescription: "Comprehensive web application for a university IEEE event using React, Next.js, MongoDB, and Tailwind CSS.",
+        fullDescription: "Designed and developed a full-stack web application for a university IEEE event. Utilized React with Next.js for server-side rendering and improved SEO. Implemented MongoDB for efficient data management and retrieval. Styled with Tailwind CSS for a responsive and modern design. Created full-stack solutions including event registration, schedule management, and participant tracking.",
+        githubLink: "https://github.com/FahdAzouz/ieee-event-website",
+        completed: false
+    },
+    {
+        title: "Personal Website for Client",
+        slug: "personal-website-client",
+        image: "/images/projects/projectBgFiller2.png",
+        shortDescription: "Custom personal website using Astro, achieving a 95% PageSpeed Insights score.",
+        fullDescription: "Created a custom personal website using Astro, a modern static site generator. Leveraged Astro's partial hydration for optimal performance and interactivity. Designed an intuitive and visually appealing interface. Implemented a content management system allowing easy updates and maintenance by the client. Optimized website performance, achieving a 95% PageSpeed Insights score for both mobile and desktop.",
+        githubLink: "https://github.com/FahdAzouz/personal-website-client",
+        completed: false
+    },
+    {
+        title: "Finance Web Application",
+        slug: "finance-web-app",
+        image: "/images/projects/finance-app.png",
+        shortDescription: "Full-stack e-commerce application using React TypeScript, Django, and Firebase.",
+        fullDescription: "Developed a full-stack e-commerce application using React TypeScript, Django, and Firebase. Implemented responsive UI/UX design, increasing mobile user engagement by 40%. Integrated a secure payment gateway, resulting in a 99.9% successful transaction rate.",
+        githubLink: "https://github.com/yibork/dashboard-finance-derivatives",
+        completed: true
+    },
+    {
+        title: "CampusCompass",
+        slug: "campus-compass",
+        image: "/images/projects/campus-compass.jpeg",
+        shortDescription: "React Native mobile app to coordinate between counselors and students.",
+        fullDescription: "Designed a React Native mobile app to coordinate between counselors and students. Increased student-counselor engagement by 60% through intuitive interface design.",
+        githubLink: "https://github.com/FahdAzouz/CampusCompass",
+        completed: true
+    },
+    {
+        title: "AirBnB Clone",
+        slug: "airbnb-clone",
+        image: "/images/projects/airbnb-clone.png",
+        shortDescription: "Web app with Flask, Python, jQuery, and RESTful methodology.",
+        fullDescription: "Developed a web application with Flask, Python, jQuery, and RESTful methodology. Achieved 95% code test coverage and reduced page load time by 30%.",
+        githubLink: "https://github.com/FahdAzouz/AirBnB_clone_v4",
+        completed: true
+    },
+    {
+        title: "CNN Image Classification",
+        slug: "cnn-image-classification",
+        image: "/images/projects/cnn-classification.png",
+        shortDescription: "Image classification model achieving 92% accuracy on test data.",
+        fullDescription: "Implemented an image classification model achieving 92% accuracy on test data. Optimized model architecture, reducing inference time by 25%.",
+        githubLink: "https://github.com/FahdAzouz/CNN_image_classification",
+        completed: true
+    },
+    {
+        title: "BustTheGhost",
+        slug: "bust-the-ghost",
+        image: "/images/projects/bust-the-ghost.png",
+        shortDescription: "AI-driven game using Bayesian inference for ghost location estimation.",
+        fullDescription: "Developed an AI-driven game using Bayesian inference for ghost location estimation. The game implements advanced AI concepts to predict and locate a ghost within a virtual environment. Through iterative algorithm refinement, I improved the prediction accuracy by 35%, enhancing the game's challenge and realism. This project showcases my skills in AI development, probability theory, and game design, while also demonstrating my ability to optimize complex algorithms for better performance.",
+        githubLink: "https://github.com/FahdAzouz/BustTheGhost",
+        completed: true
+    },
+
+];
